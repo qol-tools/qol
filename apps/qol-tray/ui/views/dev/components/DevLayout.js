d439346cf2f6e52c34729507797e65af252f1eb7
KMRH47 <kmrh47@proton.me>
2026-06-07 07:14:50 +0200

    feat(qol-tray): add Core section with global ghost UI opacity slider

--- apps/qol-tray/ui/views/dev/components/DevLayout.js
+++ apps/qol-tray/ui/views/dev/components/DevLayout.js
@@ -1,6 +1,7 @@
 import { html } from '../../../lib/html.js';
 import { PageShell } from '../../../components/PageShell.js';
 import { PluginsSection } from './PluginsSection.js';
+import { CoreSection } from './CoreSection.js';
 import { CoreLogSection } from './CoreLogSection.js';
 import { ActionsSection } from './ActionsSection.js';
 import { ToolingGhAccountSection } from './ToolingGhAccountSection.js';
@@ -13,6 +14,7 @@ export function DevLayout({ ctrl, containerRef }) {
                     <${PluginsSection} ctrl=${ctrl} />
                 </div>
                 <div class="dev-col-secondary">
+                    <${CoreSection} />
                     <${CoreLogSection} ctrl=${ctrl} />
                     <${ActionsSection} ctrl=${ctrl} />
                     <${ToolingGhAccountSection} />
