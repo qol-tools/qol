68d8cdc71e54b875820f3dce2ac017643c9dc207
kmrh47 <kmrh47@proton.me>
2026-06-12 10:30:51 +0200

    refactor(qol-cli): drive health and emu probes through pollers

--- tools/qol-cli/src/poller.rs
+++ tools/qol-cli/src/poller.rs
@@ -11,6 +11,7 @@ impl<T: Send + 'static> Poller<T> {
         Self::spawn_with_backoff(interval, interval, work, |_| true)
     }
 
+    #[allow(dead_code)]
     pub(crate) fn spawn_adaptive(
         base: Duration,
         cap: Duration,
